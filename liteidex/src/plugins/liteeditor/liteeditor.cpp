3961700c586b86549cd90e8587c66dc9b883ed2c
visualfc <visualfc@gmail.com>
2011-06-27 14:27:08 +0800

    update file license

--- liteidex/src/plugins/liteeditor/liteeditor.cpp
+++ liteidex/src/plugins/liteeditor/liteeditor.cpp
@@ -21,7 +21,7 @@
 // Module: liteeditor.cpp
 // Creator: visualfc <visualfc@gmail.com>
 // date: 2011-3-26
-// $Id: liteeditor.cpp,v 1.0 2011-5-12 visualfc Exp $
+// $Id: liteeditor.cpp,v 1.0 2011-6-27 visualfc Exp $
 
 #include "liteeditor.h"
 #include "liteeditorwidget.h"
--- liteidex/src/plugins/liteeditor/liteeditor.h
+++ liteidex/src/plugins/liteeditor/liteeditor.h
@@ -21,7 +21,7 @@
 // Module: liteeditor.h
 // Creator: visualfc <visualfc@gmail.com>
 // date: 2011-3-26
-// $Id: liteeditor.h,v 1.0 2011-5-12 visualfc Exp $
+// $Id: liteeditor.h,v 1.0 2011-6-27 visualfc Exp $
 
 #ifndef LITEEDITOR_H
 #define LITEEDITOR_H
