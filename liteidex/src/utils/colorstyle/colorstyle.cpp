46e9f7c1e8318fa9307090ce9a2461e963f6b911
visualfc <visualfc@gmail.com>
2011-07-06 15:42:36 +0800

    update .cpp memory_check

--- liteidex/src/utils/colorstyle/colorstyle.cpp
+++ liteidex/src/utils/colorstyle/colorstyle.cpp
@@ -26,6 +26,15 @@
 #include "colorstyle.h"
 #include <QFile>
 #include <QXmlStreamReader>
+//lite_memory_check_begin
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+     #define _CRTDBG_MAP_ALLOC
+     #include <stdlib.h>
+     #include <crtdbg.h>
+     #define DEBUG_NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
+     #define new DEBUG_NEW
+#endif
+//lite_memory_check_end
 
 ColorStyleScheme::ColorStyleScheme(QObject *parent)
     : QObject(parent)
