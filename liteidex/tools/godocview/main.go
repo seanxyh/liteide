e7fb350b712dd37f543ab08e1efdba55ac057652
visualfc <visualfc@gmail.com>
2011-07-21 15:44:16 +0800

    tools/godocview pkgname fuzzy query

--- liteidex/tools/godocview/main.go
+++ liteidex/tools/godocview/main.go
@@ -6,22 +6,29 @@ import (
 	"path/filepath"
 	"flag"
 	"runtime"
+	"strings"
 )
 
 var (
 	goroot = flag.String("goroot", runtime.GOROOT(), "Go root directory")
 	subdir = flag.String("subdir", "src/pkg", "Go package subdir")
-	html    = flag.Bool("html", false, "print HTML in command-line mode")
+	find   = flag.String("find", "", "find package name")
+	html   = flag.Bool("html", false, "print HTML in command-line mode")
 )
 
 func main() {
-	flag.Parse()	
+	flag.Parse()
 	var template string
 	if *html {
 		template = packageHTML
 	} else {
 		template = packageText
 	}
-	contents := GetPkgList(filepath.Join(*goroot,*subdir),template)
-	fmt.Println(string(contents))
+	if len(*find) > 0 {
+		best, matchs := FindPkgList(filepath.Join(*goroot, *subdir), *find)
+		fmt.Printf("%s:%s",best,strings.Join(matchs,","))
+	} else {
+		contents := GetPkgList(filepath.Join(*goroot, *subdir), template)
+		fmt.Println(string(contents))
+	}
 }
--- liteidex/tools/godocview/pkglist.go
+++ liteidex/tools/godocview/pkglist.go
@@ -9,10 +9,12 @@ import (
 	"io"
 	"time"
 	"strconv"
+	"strings"
+	"path/filepath"
 )
 
 var (
-	fs     FileSystem = OS// the underlying file system
+	fs FileSystem = OS // the underlying file system
 )
 
 // Fake package file and name for commands. Contains the command documentation.
@@ -28,7 +30,7 @@ func textFmt(w io.Writer, format string, x ...interface{}) {
 
 func htmlEscFmt(w io.Writer, format string, x ...interface{}) {
 	switch v := x[0].(type) {
-	case int:		
+	case int:
 		template.HTMLEscape(w, []byte(strconv.Itoa(v)))
 	case []byte:
 		template.HTMLEscape(w, v)
@@ -54,10 +56,10 @@ func timeFmt(w io.Writer, format string, x ...interface{}) {
 }
 
 var fmap = template.FormatterMap{
-	"":             textFmt,
-	"html-esc":     htmlEscFmt,
-	"padding":      paddingFmt,	
-	"time":         timeFmt,
+	"":         textFmt,
+	"html-esc": htmlEscFmt,
+	"padding":  paddingFmt,
+	"time":     timeFmt,
 }
 
 func readTemplateData(data string) *template.Template {
@@ -88,19 +90,61 @@ func applyTemplate(t *template.Template, name string, data interface{}) []byte {
 	return buf.Bytes()
 }
 
-type Info struct{
+type Info struct {
 	Dirs *DirList
-}	
+}
 
-func GetPkgList(root string,templateData string) []byte {	
+func (dir *Directory) lookupPkg(pkgname string) string {
+	p := []string{pkgname}
+	if strings.Contains(pkgname, `\`) {
+		p = strings.Split(pkgname, `\`)
+	} else if strings.Contains(pkgname, `/`) {
+		p = strings.Split(pkgname, `/`)
+	}
+	fmt.Println(p)
+	for i := 0; i < len(p); i++ {
+		for j := 0; j < len(dir.Dirs); j++ {
+			if dir.Dirs[j].Name == p[i] {
+				dir = dir.Dirs[j]
+				fmt.Println(dir.Path)
+				break
+			}
+		}
+	}
+	return dir.Name
+}
+
+func FindPkgList(root string, pkgname string) (best string, matchs []string) {
+	dir := newDirectory(root, nil, -1)
+	if dir == nil {
+		return
+	}
+	info := Info{Dirs: dir.listing(true)}
+	max := len(info.Dirs.List)
+	for i := 0; i < max; i++ {
+		name := info.Dirs.List[i].Name
+		path := filepath.ToSlash(info.Dirs.List[i].Path)
+
+		if name == pkgname || path == pkgname {
+			best = path
+		} else if strings.HasSuffix(path, "/"+pkgname) {
+			best = path
+		} else if strings.Contains(path, pkgname) {
+			matchs = append(matchs, path)
+		}
+	}
+	return
+}
+
+func GetPkgList(root string, templateData string) []byte {
 	data := readTemplateData(templateData)
 	if data == nil {
 		return nil
-	}		
-	dir := newDirectory(root,nil,-1)
+	}
+	dir := newDirectory(root, nil, -1)
 	if dir == nil {
 		return nil
 	}
-	info := Info{Dirs:dir.listing(true)}
-	return applyTemplate(data,"package",info)
-}	
+	info := Info{Dirs: dir.listing(true)}
+	return applyTemplate(data, "package", info)
+}
