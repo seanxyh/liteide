f1b2fddd2564cbf30186e3076d609434223f875f
visualfc <visualfc@gmail.com>
2011-07-21 21:14:57 +0800

    tools/godocview use template

--- liteidex/tools/godocview/main.go
+++ liteidex/tools/godocview/main.go
@@ -6,29 +6,54 @@ import (
 	"path/filepath"
 	"flag"
 	"runtime"
-	"strings"
+	"os"
 )
 
 var (
 	goroot = flag.String("goroot", runtime.GOROOT(), "Go root directory")
-	subdir = flag.String("subdir", "src/pkg", "Go package subdir")
-	find   = flag.String("find", "", "find package name")
-	html   = flag.Bool("html", false, "print HTML in command-line mode")
+	subdir = flag.String("subdir", "src/pkg", "Go package sub directory")
+	find   = flag.String("find", "", "find package list, :pkg flag is best match")
+	html   = flag.Bool("html", false, "Print HTML mode")
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"usage: godocview -find pkgname\n"+
+			"       godocview -find *\t:list all package\n"+
+			"       godocview -find build\t:find go/build package\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
+	flag.Usage = usage
 	flag.Parse()
+
+	if len(*find) == 0 {
+		flag.Usage()
+	}
+
+	info := FindPkgInfo(filepath.Join(*goroot, *subdir), *find)
+	if info == nil {
+		os.Exit(2)
+	}
+
 	var template string
-	if *html {
-		template = packageHTML
+
+	if *find == "*" {
+		if *html == true {
+			template = listHTML
+		} else {
+			template = listText
+		}
 	} else {
-		template = packageText
+		if *html == true {
+			template = findHTML
+		} else {
+			template = findText
+		}
 	}
-	if len(*find) > 0 {
-		best, matchs := FindPkgList(filepath.Join(*goroot, *subdir), *find)
-		fmt.Printf("%s:%s",best,strings.Join(matchs,","))
-	} else {
-		contents := GetPkgList(filepath.Join(*goroot, *subdir), template)
-		fmt.Println(string(contents))
-	}
+	
+	contents := info.GetPkgList(template)
+	fmt.Println(string(contents))
 }
