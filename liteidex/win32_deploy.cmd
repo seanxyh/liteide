d44bbd2d53504b1aaeb508a99e7130948be84089
visualfc <visualfc@gmail.com>
2011-07-25 18:47:15 +0800

    os_deploy: seperated environment and build

--- liteidex/win32_deploy.cmd
+++
@@ -1,8 +0,0 @@
-@echo on
-copy LICENSE.LGPL liteide
-copy LGPL_EXCEPTION.TXT liteide
-copy README.TXT liteide
-copy CHANGES.TXT liteide 
-copy GOCODE.TXT liteide
-xcopy deploy liteide  /e /y /i
-xcopy os_deploy\win32 liteide /e /y  /i
---
+++ liteidex/windows_deploy.cmd
@@ -0,0 +1,8 @@
+@echo on
+copy LICENSE.LGPL liteide
+copy LGPL_EXCEPTION.TXT liteide
+copy README.TXT liteide
+copy CHANGES.TXT liteide 
+copy GOCODE.TXT liteide
+xcopy deploy liteide  /e /y /i
+xcopy os_deploy\windows liteide /e /y  /i
