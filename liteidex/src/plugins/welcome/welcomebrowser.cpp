0ce561e09c23e9aef97831bfa3d2161df04a1d4a
visualfc <visualfc@gmail.com>
2011-07-27 09:24:59 +0800

    welcome: use treeview

--- liteidex/src/plugins/welcome/welcomebrowser.cpp
+++ liteidex/src/plugins/welcome/welcomebrowser.cpp
@@ -58,32 +58,34 @@ WelcomeBrowser::WelcomeBrowser(LiteApi::IApplication *app)
 
     ui->setupUi(m_widget);
 
-    m_recentProjectsModel = new QStringListModel(this);
-    ui->recentProjectslistView->setModel(m_recentProjectsModel);
-    m_recentProjectsModel->setStringList(m_liteApp->fileManager()->recentProjects());
-    ui->recentProjectslistView->setEditTriggers(0);
+    m_recentProjectsModel = new QStandardItemModel(0,2,this);
+    ui->recentProjectsTreeView->setModel(m_recentProjectsModel);
+    ui->recentProjectsTreeView->setRootIsDecorated(false);
+    ui->recentProjectsTreeView->setEditTriggers(0);
+    ui->recentProjectsTreeView->setHeaderHidden(true);
+    ui->recentProjectsTreeView->setTextElideMode(Qt::ElideMiddle);
+    ui->recentProjectsTreeView->header()->setResizeMode(0,QHeaderView::ResizeToContents);
+    //ui->recentProjectsTreeView->header()->setResizeMode(1,QHeaderView::Stretch);
+    //ui->recentProjectsTreeView->header()->setStretchLastSection(true);
 
-    m_recentFilesModel = new QStringListModel(this);
-    ui->recentFileslistView->setModel(m_recentFilesModel);
-    m_recentFilesModel->setStringList(m_liteApp->fileManager()->recentFiles());
-    ui->recentFileslistView->setEditTriggers(0);
+    m_recentFilesModel = new QStandardItemModel(0,2,this);
+    ui->recentFilesTreeView->setModel(m_recentProjectsModel);
+    ui->recentFilesTreeView->setRootIsDecorated(false);
+    ui->recentFilesTreeView->setEditTriggers(0);
+    ui->recentFilesTreeView->setHeaderHidden(true);
+    ui->recentFilesTreeView->setTextElideMode(Qt::ElideMiddle);
+    ui->recentFilesTreeView->header()->setResizeMode(QHeaderView::ResizeToContents);
 
     m_docModel = new QStandardItemModel(0,2,this);
     ui->docTreeView->setModel(m_docModel);
+    ui->docTreeView->setRootIsDecorated(false);
     ui->docTreeView->setEditTriggers(0);
     ui->docTreeView->setHeaderHidden(true);
+    ui->docTreeView->setTextElideMode(Qt::ElideMiddle);
     ui->docTreeView->header()->setResizeMode(0,QHeaderView::ResizeToContents);
 
-    QDir dir(m_liteApp->resourcePath()+"/liteidedoc");
-    QFileInfoList infoList = dir.entryInfoList(QDir::Files|QDir::NoSymLinks);
-    foreach (QFileInfo info, infoList) {
-        m_docModel->appendRow(QList<QStandardItem*>()
-                              << new QStandardItem(info.baseName())
-                              << new QStandardItem(info.filePath()));
-    }
-
     m_docBrowser = new DocumentBrowser(m_liteApp);
-    m_docBrowser->setDisplayName(tr("LiteDocBrowser"));
+    m_docBrowser->setDisplayName(tr("DocBrowser"));
     BrowserEditorManager *browserManger = LiteApi::findExtensionObject<BrowserEditorManager*>(m_liteApp,"LiteApi.BrowserEditorManager");
     if (browserManger) {
         m_browserAct = browserManger->addBrowser(m_docBrowser);
@@ -93,12 +95,16 @@ WelcomeBrowser::WelcomeBrowser(LiteApi::IApplication *app)
     connect(ui->openFileButton,SIGNAL(clicked()),m_liteApp->fileManager(),SLOT(openFiles()));
     connect(ui->openProjectButton,SIGNAL(clicked()),m_liteApp->fileManager(),SLOT(openProjects()));
     connect(ui->openEditorButton,SIGNAL(clicked()),m_liteApp->fileManager(),SLOT(openEditors()));
-    connect(ui->recentProjectslistView,SIGNAL(doubleClicked(QModelIndex)),this,SLOT(openRecentProjectList(QModelIndex)));
-    connect(ui->recentFileslistView,SIGNAL(doubleClicked(QModelIndex)),this,SLOT(openRecentFileList(QModelIndex)));
-    connect(m_liteApp->fileManager(),SIGNAL(recentProjectsChanged()),this,SLOT(recentProjectsChanged()));
-    connect(m_liteApp->fileManager(),SIGNAL(recentFilesChanged()),this,SLOT(recentFilesChanged()));
+    connect(ui->recentProjectsTreeView,SIGNAL(doubleClicked(QModelIndex)),this,SLOT(openRecentProject(QModelIndex)));
+    connect(ui->recentFilesTreeView,SIGNAL(doubleClicked(QModelIndex)),this,SLOT(openRecentFile(QModelIndex)));
+    connect(m_liteApp->fileManager(),SIGNAL(recentProjectsChanged()),this,SLOT(loadRecentProjects()));
+    connect(m_liteApp->fileManager(),SIGNAL(recentFilesChanged()),this,SLOT(loadRecentFiles()));
     connect(ui->optionsButton,SIGNAL(clicked()),m_liteApp->optionManager(),SLOT(exec()));
     connect(ui->docTreeView,SIGNAL(doubleClicked(QModelIndex)),this,SLOT(openLiteDoument(QModelIndex)));
+
+    loadRecentProjects();
+    loadRecentFiles();
+    loadDocFiles();
 }
 
 WelcomeBrowser::~WelcomeBrowser()
@@ -112,32 +118,76 @@ QWidget *WelcomeBrowser::widget()
     return m_widget;
 }
 
-void WelcomeBrowser::openRecentProjectList(QModelIndex index)
+static void resizeTreeView(QTreeView *treeView)
+{
+    treeView->resizeColumnToContents(0);
+}
+
+void WelcomeBrowser::loadDocFiles()
+{
+    m_docModel->clear();
+    QDir dir(m_liteApp->resourcePath()+"/liteidedoc");
+    QFileInfoList infoList = dir.entryInfoList(QDir::Files|QDir::NoSymLinks);
+    foreach (QFileInfo info, infoList) {
+        m_docModel->appendRow(QList<QStandardItem*>()
+                              << new QStandardItem(info.baseName())
+                              << new QStandardItem(info.filePath()));
+    }
+    resizeTreeView(ui->docTreeView);
+}
+
+void WelcomeBrowser::loadRecentProjects()
+{
+    m_recentProjectsModel->clear();
+    QStringList recentProjects = m_liteApp->fileManager()->recentProjects();
+    foreach (QString file, recentProjects) {
+        QFileInfo info(file);
+        m_recentProjectsModel->appendRow(QList<QStandardItem*>()
+                              << new QStandardItem(info.fileName())
+                              << new QStandardItem(info.filePath()));
+    }
+    resizeTreeView(ui->recentProjectsTreeView);
+}
+
+void WelcomeBrowser::loadRecentFiles()
+{
+    m_recentFilesModel->clear();
+    QStringList files = m_liteApp->fileManager()->recentFiles();
+    foreach (QString file, files) {
+        QFileInfo info(file);
+        m_recentFilesModel->appendRow(QList<QStandardItem*>()
+                              << new QStandardItem(info.fileName())
+                              << new QStandardItem(info.filePath()));
+    }
+    resizeTreeView(ui->recentFilesTreeView);
+}
+
+
+void WelcomeBrowser::openRecentProject(QModelIndex index)
 {
     if (!index.isValid()) {
         return;
     }
-    QString fileName = index.data().toString();
+    QModelIndex i = m_recentProjectsModel->index(index.row(),1);
+    if (!i.isValid()) {
+        return;
+    }
+    QString fileName = i.data().toString();
     m_liteApp->fileManager()->openProject(fileName);
 }
 
-void WelcomeBrowser::recentProjectsChanged()
-{
-    m_recentProjectsModel->setStringList(m_liteApp->fileManager()->recentProjects());
-}
 
-void WelcomeBrowser::openRecentFileList(QModelIndex index)
+void WelcomeBrowser::openRecentFile(QModelIndex index)
 {
     if (!index.isValid()) {
         return;
     }
-    QString fileName = index.data().toString();
+    QModelIndex i = m_recentProjectsModel->index(index.row(),1);
+    if (!i.isValid()) {
+        return;
+    }
+    QString fileName = i.data().toString();
     m_liteApp->fileManager()->openEditor(fileName);
-}
-
-void WelcomeBrowser::recentFilesChanged()
-{
-    m_recentFilesModel->setStringList(m_liteApp->fileManager()->recentFiles());
 }
 
 void WelcomeBrowser::openLiteDoument(QModelIndex index)
@@ -156,7 +206,16 @@ void WelcomeBrowser::openLiteDoument(QModelIndex index)
     }
     QByteArray data = f.readAll();
     f.close();
-    m_docBrowser->browser()->setText(QString::fromUtf8(data,data.size()));
+    if (data.isEmpty()) {
+        return;
+    }
+    m_docBrowser->setDisplayName(info.fileName());
+    if (info.suffix().toLower() == ".html" ||
+            info.suffix().toLower() == ".htm") {
+        m_docBrowser->browser()->setText(QString::fromUtf8(data,data.size()));
+    } else {
+        m_docBrowser->browser()->setText(QString::fromUtf8(data,data.size()));
+    }
 
     BrowserEditorManager *browserManger = LiteApi::findExtensionObject<BrowserEditorManager*>(m_liteApp,"LiteApi.BrowserEditorManager");
     if (browserManger) {
