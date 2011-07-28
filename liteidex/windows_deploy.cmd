81cef933c1ac8b047e8bf1f99efc6e1c42064e62
visualfc <visualfc@gmail.com>
2011-07-28 15:28:19 +0800

    update doc and deploy.sh

--- liteidex/windows_deploy.cmd
+++ liteidex/windows_deploy.cmd
@@ -1,8 +1,5 @@
 @echo on
 copy LICENSE.LGPL liteide
 copy LGPL_EXCEPTION.TXT liteide
-copy README.TXT liteide
-copy CHANGES.TXT liteide 
-copy GOCODE.TXT liteide
 xcopy deploy liteide  /e /y /i
 xcopy os_deploy\windows liteide /e /y  /i
