e14a639e44dc2656628258fc4907ad6a03d47536
visualfc <visualfc@gmail.com>
2011-12-15 10:21:56 +0800

    windows add bash.bat

--- liteidex/windows_deploy.cmd
+++ liteidex/windows_deploy.cmd
@@ -2,5 +2,6 @@
 copy LICENSE.LGPL liteide
 copy LGPL_EXCEPTION.TXT liteide
 copy README.TXT liteide
+copy bash.bast liteide\bin
 xcopy deploy liteide\share\liteide  /e /y /i
 xcopy os_deploy\windows liteide\share\liteide /e /y  /i
