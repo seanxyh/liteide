3d1d61a8ff08d57090b3427ff2c62467c7687eb6
visualfc <visualfc@gmail.com>
2012-02-28 19:38:21 +0800

    up for lua tool

--- liteidex/src/plugins/golangtool/gopathproject.cpp
+++ liteidex/src/plugins/golangtool/gopathproject.cpp
@@ -1,3 +1,28 @@
+/**************************************************************************
+** This file is part of LiteIDE
+**
+** Copyright (c) 2011 LiteIDE Team. All rights reserved.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** In addition, as a special exception,  that plugins developed for LiteIDE,
+** are allowed to remain closed sourced and can be distributed under any license .
+** These rights are included in the file LGPL_EXCEPTION.txt in this package.
+**
+**************************************************************************/
+// Module: gopathproject.cpp
+// Creator: visualfc <visualfc@gmail.com>
+// date: 2012-2-27
+// $Id: gopathproject.cpp,v 1.0 2012-2-28 visualfc Exp $
+
 #include "gopathproject.h"
 #include "gopathmodel.h"
 #include "liteapi/litefindobj.h"
@@ -9,6 +34,15 @@
 #include <QDir>
 #include <QStringList>
 #include <QDebug>
+//lite_memory_check_begin
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+     #define _CRTDBG_MAP_ALLOC
+     #include <stdlib.h>
+     #include <crtdbg.h>
+     #define DEBUG_NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
+     #define new DEBUG_NEW
+#endif
+//lite_memory_check_end
 
 GopathProject::GopathProject(GopathBrowser *browser) : m_browser(browser)
 {
