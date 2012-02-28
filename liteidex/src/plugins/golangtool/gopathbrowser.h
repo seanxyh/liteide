d71a0ac3f98b3f7bcd9981961bece772d08db2a1
visualfc <visualfc@gmail.com>
2012-02-28 19:29:39 +0800

    golangtool : treeview context menu

--- liteidex/src/plugins/golangtool/gopathbrowser.h
+++ liteidex/src/plugins/golangtool/gopathbrowser.h
@@ -5,6 +5,8 @@
 #include "liteapi/liteapi.h"
 #include "liteenvapi/liteenvapi.h"
 #include <QModelIndex>
+#include <QFileInfo>
+#include <QDir>
 
 class QTreeView;
 class GopathModel;
@@ -27,14 +29,43 @@ public slots:
     void openPathIndex(const QModelIndex &index);
     void reloadEnv();
     void currentEditorChanged(LiteApi::IEditor*);
+    void treeViewContextMenuRequested(const QPoint &pos);
+    void openEditor();
+    void newFile();
+    void newFileWizard();
+    void renameFile();
+    void removeFile();
+    void newFolder();
+    void renameFolder();
+    void removeFolder();
+    void openShell();
+    void openExplorer();
 signals:
     void startPathChanged(const QString& path);
+protected:
+    QFileInfo contextFileInfo() const;
+    QDir contextDir() const;
+    static QString getShellCmd(LiteApi::IApplication *app);
+    static QStringList getShellArgs(LiteApi::IApplication *app);
 private:
     LiteApi::IApplication *m_liteApp;
     QWidget *m_widget;
     QTreeView *m_pathTree;
     GopathModel *m_model;
     QStringList m_pathList;
+    QFileInfo m_contextInfo;
+    QMenu   *m_fileMenu;
+    QMenu   *m_folderMenu;
+    QAction *m_openEditorAct;
+    QAction *m_newFileAct;
+    QAction *m_newFileWizardAct;
+    QAction *m_removeFileAct;
+    QAction *m_renameFileAct;
+    QAction *m_newFolderAct;
+    QAction *m_removeFolderAct;
+    QAction *m_renameFolderAct;
+    QAction *m_openShellAct;
+    QAction *m_openExplorerAct;
 };
 
 #endif // GOPATHBROWSER_H
