1ba9e2094ee38fb1add601f4d97c5a0222382a61
visualfc <visualfc@gmail.com>
2012-06-27 15:34:34 +0800

    update build.cmd

--- build/build.cmd
+++ build/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set BUILD_ROOT=%CD%
-if not x%LITEIDE_ROOT%==x set LITEIDE_ROOT=%CD%\..\liteidex
+if x%LITEIDE_ROOT%==x set LITEIDE_ROOT=%CD%\..\liteidex
 set OLD_GOPATH=%GOPATH%
 
 echo build liteide 
@@ -11,20 +11,28 @@ echo BUILD_ROOT=%BUILD_ROOT%
 echo LITEIDE_ROOT=%LITEIDE_ROOT%
 echo .
 
-if x%QTDIR%==x goto fail
+if x%QTDIR%==x goto qtdir_fail
 
 echo qmake liteide ...
 echo .
-call qmake ../liteidex
+call qmake %LITEIDE_ROOT%
+
+if ERRORLEVEL 1 goto qmake_fail
 
 echo make liteide ...
 echo .
 call make release
 
+if ERRORLEVEL 1 goto make_fail
+
+call go version
+
+if ERRORLEVEL 1 goto go_fail
+
 echo build liteide tools
 echo .
 
-cd ../liteidex
+cd %LITEIDE_ROOT%
 set GOPATH=%CD%
 go install -v tools/goastview
 go install -v tools/godocview
@@ -42,27 +50,40 @@ if not exist liteide\lib mkdir liteide\lib
 if not exist liteide\lib\liteide mkdir liteide\lib\liteide
 if not exist liteide\lib\liteide\plugins mkdir liteide\lib\liteide\plugins
 
-xcopy /y /i %LITEIDE_ROOT%\LICENSE.LGPL liteide
-xcopy /y /i %LITEIDE_ROOT%\LGPL_EXCEPTION.TXT liteide
-xcopy /y /i %LITEIDE_ROOT%\README.TXT liteide
+xcopy %LITEIDE_ROOT%\LICENSE.LGPL liteide /y
+xcopy %LITEIDE_ROOT%\LGPL_EXCEPTION.TXT liteide /y
+xcopy %LITEIDE_ROOT%\README.TXT liteide /y
 
-xcopy /y /i %LITEIDE_ROOT%\liteide\bin\* liteide\bin
-xcopy /y /i %LITEIDE_ROOT%\bin\* liteide\bin
-xcopy /y /i %LITEIDE_ROOT%\liteide\lib\liteide\plugins\*.dll liteide\lib\liteide\plugins
+xcopy %LITEIDE_ROOT%\liteide\bin\* liteide\bin /y
+xcopy %LITEIDE_ROOT%\bin\* liteide\bin /y
+xcopy %LITEIDE_ROOT%\liteide\lib\liteide\plugins\*.dll liteide\lib\liteide\plugins /y
 
-xcopy /e /y /i %LITEIDE_ROOT%\deploy liteide\share\liteide
-xcopy /e /y /i %LITEIDE_ROOT%\os_deploy\windows liteide\share\liteide
+xcopy %LITEIDE_ROOT%\deploy liteide\share\liteide /e /y /i 
+xcopy %LITEIDE_ROOT%\os_deploy\windows liteide\share\liteide  /e /y /i 
 
-xcopy /y /i %QTDIR%\bin\QtCore4.dll liteide\bin
-xcopy /y /i %QTDIR%\bin\QtGui4.dll liteide\bin
-xcopy /y /i %QTDIR%\bin\QtXml4.dll liteide\bin
-xcopy /y /i %QTDIR%\bin\mingwm10.dll liteide\bin
-xcopy /y /i %QTDIR%\bin\libgcc_s_dw2-1.dll liteide\bin
+xcopy %QTDIR%\bin\QtCore4.dll liteide\bin /y
+xcopy %QTDIR%\bin\QtGui4.dll liteide\bin /y 
+xcopy %QTDIR%\bin\QtXml4.dll liteide\bin /y 
+xcopy %QTDIR%\bin\mingwm10.dll liteide\bin /y
+xcopy %QTDIR%\bin\libgcc_s_dw2-1.dll liteide\bin /y
 
 goto end
 
-:fail
-echo build fail
+:qtdir_fail
+echo error, QTDIR is null
+goto end
+
+:qmake_fail
+echo error, qmake fail
+goto end
+
+:make_fail
+echo error, make fail
+goto end
+
+:go_fail
+echo error, go fail
+goto end
 
 :end
 set GOPATH=%OLD_GOPATH%
