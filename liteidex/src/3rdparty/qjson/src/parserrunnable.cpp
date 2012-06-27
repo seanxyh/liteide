e1ad3dda66e99ef0f4f09549c4d0e45178a49a69
visualfc <visualfc@gmail.com>
2012-06-27 19:37:49 +0800

    update doc

--- liteidex/src/3rdparty/qjson/src/parserrunnable.cpp
+++ liteidex/src/3rdparty/qjson/src/parserrunnable.cpp
@@ -52,17 +52,17 @@ void ParserRunnable::setData( const QByteArray& data ) {
 
 void ParserRunnable::run()
 {
-  qDebug() << Q_FUNC_INFO;
+  //qDebug() << Q_FUNC_INFO;
 
   bool ok;
   Parser parser;
   QVariant result = parser.parse (d->m_data, &ok);
   if (ok) {
-    qDebug() << "successfully converted json item to QVariant object";
+    //qDebug() << "successfully converted json item to QVariant object";
     emit parsingFinished(result, true, QString());
   } else {
     const QString errorText = tr("An error occurred while parsing json: %1").arg(parser.errorString());
-    qCritical() << errorText;
+    //qCritical() << errorText;
     emit parsingFinished(QVariant(), false, errorText);
   }
 }
