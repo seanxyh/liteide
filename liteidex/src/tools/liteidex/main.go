dbd693cf9f9a2b5827981f03f6a1e3077355b834
visualfc <visualfc@gmail.com>
2012-11-17 18:43:49 +0800

    goproxy : add setenv

--- liteidex/src/tools/liteidex/main.go
+++ liteidex/src/tools/liteidex/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"os"
+	"strings"
 )
 
 func main() {
 	liteide(os.Args)
 }
 
+var (
+	Env = os.Environ()
+)
+
 func init() {
 	RegCmd("version", func(args []byte) (error, []byte) {
 		return nil, []byte("liteidex(go) version 15.0")
+	})
+	RegCmd("setenv", func(args []byte) (error, []byte) {
+		Env = strings.Split(string(args), "\n")
+		return nil, nil
 	})
 }
